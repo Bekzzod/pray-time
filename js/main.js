61164bc88f43b7e39859af94f09284ce7e70d0ca
Bekzod <bek.abakulov@gmail.com>
2022-05-22 19:35:25 +0500

    Trying to add countdown frow each namaz

--- js/main.js
+++ js/main.js
@@ -8,33 +8,35 @@ const currentDateFormatted =
 const currentTimeFormatted =
   ("0" + currentDate.getHours()).slice(-2) +
   ":" +
-  ("0" + currentDate.getMinutes()).slice(-2);
+  ("0" + currentDate.getMinutes()).slice(-2) +
+  ":" +
+  ("0" + currentDate.getSeconds()).slice(-2);
 const dateURL = `https://api.aladhan.com/v1/gToH?date=${currentDateFormatted}`;
 
 function updateClock() {
   currentDate = new Date(); // current date
-
-  document.getElementById("hours").innerHTML = (
-    "0" + currentDate.getHours()
-  ).slice(-2);
-  document.getElementById("minutes").innerHTML = (
-    "0" + currentDate.getMinutes()
-  ).slice(-2);
+  document.getElementById("currentTime").innerHTML =
+    ("0" + currentDate.getHours()).slice(-2) +
+    ":" +
+    ("0" + currentDate.getMinutes()).slice(-2) +
+    ":" +
+    ("0" + currentDate.getSeconds()).slice(-2);
 
   setTimeout(updateClock, 1000);
 }
 
 updateClock();
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  getMethods();
   getLocation(getNamazTimes);
 });
 
 function getLocation(callback) {
-  navigator.geolocation.getCurrentPosition(function(position) {
+  navigator.geolocation.getCurrentPosition(function (position) {
     const returnValue = {
       latitude: position.coords.latitude,
-      longitude: position.coords.longitude
+      longitude: position.coords.longitude,
     };
 
     callback(returnValue.latitude, returnValue.longitude);
@@ -42,38 +44,57 @@ function getLocation(callback) {
 }
 
 async function getNamazTimes(latitude, longitude) {
-  const namazTimesURL = `https://api.aladhan.com/v1/calendar?latitude=${latitude}&longitude=${longitude}&method=0&month=${currentDate.getMonth() +
-    1}&year=${currentDate.getFullYear()}`;
-  const response1 = await fetch(namazTimesURL);
-  const data1 = await response1.json();
+  console.log(Date.now());
+  const namazTimesURL = `https://api.aladhan.com/v1/timings/${Date.now()}?latitude=${latitude}&longitude=${longitude}`;
+  const apiResponse = await fetch(namazTimesURL);
+  const responseJSON = await apiResponse.json();
+  const responseData = responseJSON.data;
 
-  data1.data.forEach(day => {
-    if (day.date.gregorian.date == currentDateFormatted) {
-      const timings = {
-        fajr: day.timings.Fajr.split(" ")[0],
-        sunrise: day.timings.Sunrise.split(" ")[0],
-        dhuhr: day.timings.Dhuhr.split(" ")[0],
-        asr: day.timings.Asr.split(" ")[0],
-        maghrib: day.timings.Maghrib.split(" ")[0],
-        isha: day.timings.Isha.split(" ")[0]
-      };
+  const timings = {
+    fajr: responseData.timings.Fajr.split(" ")[0],
+    sunrise: responseData.timings.Sunrise.split(" ")[0],
+    dhuhr: responseData.timings.Dhuhr.split(" ")[0],
+    asr: responseData.timings.Asr.split(" ")[0],
+    maghrib: responseData.timings.Maghrib.split(" ")[0],
+    isha: responseData.timings.Isha.split(" ")[0],
+  };
 
-      for (let [key, value] of Object.entries(timings)) {
-        document.querySelector(`#${key} p`).innerHTML = value;
-      }
+  for (let [key, value] of Object.entries(timings)) {
+    document.querySelector(`#${key} p`).innerHTML = value;
+  }
 
-      for (let [key, value] of Object.entries(timings)) {
-        if (
-          Date.parse(`01/01/2011 ${currentTimeFormatted}`) <
-          Date.parse(`01/01/2011 ${value}`)
-        ) {
-          document.getElementById(key).style.backgroundColor =
-            "var(--secondary-text-color)";
-          break;
-        }
-        //document.getElementById("isha").style.backgroundColor =
-        //"var(--secondary-text-color)";
-      }
+  for (let [key, value] of Object.entries(timings)) {
+    let startDate = new Date().getTime()
+    let endDate = new Date(`${currentDate.getFullYear()}-${("0" + (currentDate.getMonth() + 1)).slice(-2)}-${("0" + currentDate.getDate()).slice(-2)}T${value}:00`).getTime();
+  
+    if (startDate < endDate) {
+      const diff = endDate - startDate;
+      const seconds = Math.floor(diff / 1000);
+
+      document.getElementById(key).style.backgroundColor = "var(--secondary-text-color)";
+      document.getElementById(key + "TimeLeft").innerHTML = seconds;
+      break;
+    }
+    //document.getElementById("isha").style.backgroundColor =
+    //"var(--secondary-text-color)";
+  }
+}
+
+async function getMethods() {
+  const selectElement = document.querySelector("#method");
+
+  const calculationMethodsURL = `http://api.aladhan.com/v1/methods`;
+  const response = await fetch(calculationMethodsURL);
+  const data = await response.json();
+
+  console.log(data.data);
+
+  Object.keys(data.data).forEach((key) => {
+    if (data.data[key].hasOwnProperty("name")) {
+      var opt = document.createElement("option");
+      opt.value = data.data[key].name;
+      opt.innerHTML = data.data[key].name;
+      selectElement.appendChild(opt);
     }
   });
 }
